9a499d4192204ae623b3129927c69bf5ca3b9c9b
kxx <132kxx@gmail.com>
2023-03-12 15:23:16 +0900

    Initial Commit

---
+++ LearningList/LearningListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningListApp.swift
+//  LearningList
+//
+//  Created by kxx: on 2023/03/12.
+//
+
+import SwiftUI
+
+@main
+struct LearningListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
